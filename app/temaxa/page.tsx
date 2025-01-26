0f5fe0ad17535836512985c8ab802749ef9ad658
oleksiy <oleksiycodeit@gmail.com>
2025-01-26 17:11:54 +0200

    first commita

--- app/temaxa/page.tsx
+++ app/temaxa/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+
+import { Suspense, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 interface Question {
@@ -10,10 +10,10 @@ interface Question {
   t: number;
   p?: string;
 }
-const PageB = () => {
-  const searchParams = useSearchParams()
- 
-  const search:number = Number(searchParams.get('d'))
+
+const PageBContent = () => {
+  const searchParams = useSearchParams();
+  const search: number = Number(searchParams.get("d"));
 
   const [data, setData] = useState([]);
   const [result, setRes] = useState();
@@ -24,20 +24,20 @@ const PageB = () => {
   const [paginationColors, setPaginationColors] = useState<string[]>([]);
   const [answered, setAnswered] = useState<boolean[]>(new Array(10).fill(false)); // Assuming max 10 questions for now
 
-
   useEffect(() => {
     fetch("/api/get_data")
       .then((response) => response.json())
       .then((result) => {
-        const a = Object.keys(result)
+        const a = Object.keys(result);
         setData(result[a[search]]); // set Data correctly
-        setPaginationColors(new Array(result[0].length).fill(''));
+        setPaginationColors(new Array(result[0].length).fill(""));
       })
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  }, [search]);
+
   const handleAnswerClick = (questionIndex: number, answerIndex: number, correctAnswerIndex: number) => {
     const buttons = document.querySelectorAll(`.a${questionIndex}`) as NodeListOf<HTMLButtonElement>;
-    buttons.forEach(button => {
+    buttons.forEach((button) => {
       button.disabled = true; // disable all buttons after an answer is selected
     });
 
@@ -46,32 +46,32 @@ const PageB = () => {
     const clickedButton = document.getElementById(`${questionIndex}-${answerIndex}`) as HTMLButtonElement;
 
     if (answerIndex + 1 === correctAnswerIndex) {
-      clickedButton.style.backgroundColor = '#4caf50';
-      clickedButton.style.color = 'white';
+      clickedButton.style.backgroundColor = "#4caf50";
+      clickedButton.style.color = "white";
       addTrueA();
-      setPaginationColors(prevColors => {
+      setPaginationColors((prevColors) => {
         const updatedColors = [...prevColors];
-        updatedColors[questionIndex] = 'bg-green-500';
+        updatedColors[questionIndex] = "bg-green-500";
         return updatedColors;
       });
-      setAnswered(prev => {
+      setAnswered((prev) => {
         const updatedAnswered = [...prev];
         updatedAnswered[questionIndex] = true;
         return updatedAnswered;
       });
     } else {
-      clickedButton.style.backgroundColor = '#f44336';
-      clickedButton.style.color = 'white';
+      clickedButton.style.backgroundColor = "#f44336";
+      clickedButton.style.color = "white";
       addFalseA();
-      setPaginationColors(prevColors => {
+      setPaginationColors((prevColors) => {
         const updatedColors = [...prevColors];
-        updatedColors[questionIndex] = 'bg-red-500';
+        updatedColors[questionIndex] = "bg-red-500";
         return updatedColors;
       });
 
       const correctButton = document.getElementById(`${questionIndex}-${correctAnswerIndex - 1}`) as HTMLButtonElement;
-      correctButton.style.backgroundColor = '#4caf50';
-      correctButton.style.color = 'white';
+      correctButton.style.backgroundColor = "#4caf50";
+      correctButton.style.color = "white";
     }
   };
 
@@ -98,26 +98,23 @@ const PageB = () => {
   return (
     <>
       <div className="flex pr-10 pl-10 md:justify-center lg:justify-center border-solid rounded-sm overflow-hidden">
-        <div
-          className="flex items-center overflow-x-auto space-x-2 scrollbar-hide"
-        >
+        <div className="flex items-center overflow-x-auto space-x-2 scrollbar-hide">
           <div className="border h-10 w-20 flex justify-center items-center cursor-pointer" onClick={clickPrev}>
             &#x2190;
           </div>
           {data.map((_, i) => (
             <div
               key={i}
-              className={`border size-10 min-w-10 flex justify-center items-center ${indexa === i ? 'shadow-gray-600 shadow-inner' : ''} ${paginationColors[i]}`}
+              className={`border size-10 min-w-10 flex justify-center items-center ${indexa === i ? "shadow-gray-600 shadow-inner" : ""} ${
+                paginationColors[i]
+              }`}
               id={`p${i + 1}`}
               onClick={() => setIndex(i)}
             >
               {i + 1}
             </div>
           ))}
-          <div
-            className="border h-10 w-20 flex justify-center items-center cursor-pointer"
-            onClick={clickNext}
-          >
+          <div className="border h-10 w-20 flex justify-center items-center cursor-pointer" onClick={clickNext}>
             &#x2192;
           </div>
         </div>
@@ -127,16 +124,13 @@ const PageB = () => {
         {data.map((question: Question, index: number) => (
           <div
             className={`p-6 m-5 w-96 mx-auto bg-white rounded-xl shadow-lg flex flex-col items-center space-y-4 text-center ${
-              index === indexa ? 'block' : 'hidden'
+              index === indexa ? "block" : "hidden"
             }`}
             key={index}
           >
             {question.p ? (
               <div>
-                <img
-                  src={question.p}
-                  alt={`Photo for question ${index + 1}`}
-                />
+                <img src={question.p} alt={`Photo for question ${index + 1}`} />
               </div>
             ) : null}
             <div className="font-bold">{question.q}</div>
@@ -156,6 +150,14 @@ const PageB = () => {
         ))}
       </div>
     </>
+  );
+};
+
+const PageB = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageBContent />
+    </Suspense>
   );
 };
 
