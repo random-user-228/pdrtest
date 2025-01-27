c4a62adca0b9248d27e15e9fc90e9b4673d6f211
oleksiy <oleksiycodeit@gmail.com>
2025-01-27 18:39:16 +0200

    first commi

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,26 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><title>Тести ПДР</title></head>
+      <head><title>Тести ПДР</title>
+      {/* Manifest */}
+      <link rel="manifest" href="/manifest.json" />
+
+{/* Apple Touch Icon */}
+<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+
+{/* iOS-специфічні мета-теги */}
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+<meta name="apple-mobile-web-app-title" content="Моя PWA" />
+
+{/* Тема додатку */}
+<meta name="theme-color" content="#000000" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<link rel="apple-touch-startup-image" href="/splashscreens/splash-640x1136.png" media="(device-width: 320px)" />
+  <link rel="apple-touch-startup-image" href="/splashscreens/splash-750x1334.png" media="(device-width: 375px)" />
+  <link rel="apple-touch-startup-image" href="/splashscreens/splash-828x1792.png" media="(device-width: 414px)" />
+  <link rel="apple-touch-startup-image" href="/splashscreens/splash-1125x2436.png" media="(device-width: 375px) and (-webkit-device-pixel-ratio: 3)"
+</head>
       <body className={inter.className}>
         <NavBar/>
         {children}</body>
--- next.config.js
+++ next.config.js
@@ -6,3 +6,11 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public', // Папка, де зберігається service worker
+  disable: process.env.NODE_ENV === 'development', // Вимкнути PWA в режимі розробки
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
