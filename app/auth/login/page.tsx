134e33e78740893f00523fa745ac593e3f39c992
oleksiy <oleksiycodeit@gmail.com>
2025-01-27 18:14:15 +0200

    first commitaa

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,7 +2,11 @@ import SignIn from "@/app/components/sign_in";
 import Image from "next/image";
 export default function Page(){
     return(
-        <><Image src='/gg.png' alt="a"></Image></>
+        <>
+        <div className="flex justify-center mt-4">
+        <img src='/gg.png' alt="a" className=" w-96 "></img>
+        </div>
+        </>
     )
 
 }
