b3b340b3f4b695be9ea1b919baf266a326404add
oleksiy <oleksiycodeit@gmail.com>
2025-01-27 17:44:02 +0200

    first commitaa

--- app/components/sign_in.tsx
+++ app/components/sign_in.tsx
@@ -9,7 +9,7 @@ export default function SignIn() {
         await signIn("google",{redirectTo: '/'})
       }}
     >
-      <button type="submit" className="left-8 border-s-zinc-600 hover:bg-blend-color bg-orange-300">Signin with Google</button>
+      <button type="submit" className="left-8 bg-white border border-solid border-blue-300 rounded-md p-1 ">Увійти</button>
     </form>
   )
 } 
