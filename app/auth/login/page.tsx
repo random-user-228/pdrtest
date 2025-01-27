9f610442ea482267cbda05b423f95dbc09bf78b4
oleksiy <oleksiycodeit@gmail.com>
2025-01-27 17:59:47 +0200

    first commitaa

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,8 @@
 import SignIn from "@/app/components/sign_in";
-
+import Image from "next/image";
 export default function Page(){
     return(
-        <></>
+        <>     <Image src='/gg.png' alt="a"></Image>   </>
     )
 
 }
